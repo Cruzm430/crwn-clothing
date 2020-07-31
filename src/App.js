530858541b06f21e7feec1b75eaa00927bebe0d1
cruzm430 <mark@Marks-MBP.home>
2020-07-31 10:47:10 -0400

    firebase added, google sign in added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,47 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
+import {auth} from './firebase/firebase.utils';
+
 import HomePage from './Pages/HomePage/HomePage.component'
 import ShopPage from './Pages/ShopPage/ShopPage.component'
 import Header from './Components/Header/Header.component'
 import SignInAndSignUp from './Pages/SignInAndSignUp/SignInAndSignUp.component'
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-      <Route path='/signin' component={SignInAndSignUp}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user =>{
+      this.setState({currentUser:user})
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/SignIn/SignIn.component.jsx
+++ src/Components/SignIn/SignIn.component.jsx
@@ -4,6 +4,7 @@ import './SignIn.styles.scss';
 
 import FormInput from '../FormInput/FormInput.component';
 import CustomButton from '../CustomButton/CustomButton.component';
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
   constructor(props){
@@ -50,9 +51,10 @@ class SignIn extends React.Component{
           label='password'
           required/>
 
-          <CustomButton type='submit'>
-            Sign In
-          </CustomButton>
+          <div className='buttons'>
+          <CustomButton type='submit'>Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+          </div>
         </form>
       </div>
     )
